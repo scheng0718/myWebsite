fdee44439295bb608191d5b4a336e49bf76d4fd5
scheng0718 <53288738+scheng0718@users.noreply.github.com>
2023-04-24 19:08:08 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const navBar = document.querySelector('.navbar')
+const content = document.querySelector('#content-container')
+const footer = document.querySelector('.footer')
+
+function renderPage(event) {
+  const target = event.target
+  let rawHTML = ''
+  if (target.matches('.project-1')) {
+    window.open("https://codepen.io/scheng0718/full/rNqLdZg", "_blank");
+  } else if (target.matches('.project-2')) {
+    window.open("https://codepen.io/scheng0718/full/MWPjdOj?editors=1010", "_blank");
+  } else if (target.matches('.project-3')) {
+    window.open("https://codepen.io/scheng0718/full/yLRMopE", "_blank");
+  } else if (target.matches('.project-4')) {
+    window.open("https://codepen.io/scheng0718/full/JjmRWGG", "_blank");
+  }
+}
+
+footer.addEventListener('click', renderPage)
